8a07ef902788a384f6dce43a43b967eeeefe1411
Jackie Viola <jacquelinelviola@gmail.com>
2014-12-19 14:13:43 -0500

    Changes
    Corrected given code from Eko

---
+++ Eko_WebInterviewTest/partials/nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<div class="navbar navbar-default navbar-fixed-top header-nav" role="navigation">
+  	<div class="container-fluid">
+  	<!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+	        </button>
+		    <a class="navbar-brand" href="index_2.php">
+		      	<img src="images/LoopLogo.png" alt="Loop Logo" />
+		    </a>
+	    </div>
+		<div class="collapse navbar-collapse site-nav" role="navigation">
+		  <ul class="nav navbar-nav">
+		   	<li><a href="http://sendaloop.com/about">About Us</a></li>
+		   	<li><a href="http://www.ekoapp.com" target="_blank">Eko</a></li>
+			<!-- <li><a href="jobs">Contact</a></li> -->
+		  </ul>
+		
+		</div>
+    </div>
+</div>
