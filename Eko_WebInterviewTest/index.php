250a9038d6dd5746a7288e756c15be2fe7307bc4
Jackie Viola <jacquelinelviola@gmail.com>
2014-12-19 14:18:10 -0500

    Deleted test files. Changed index.php
    Deleted test.html and index_2.php. These files were only used to get a
    feel for the website and debugging the HTML. Made index.php the main
    page, containing the code from index_2.php.

--- Eko_WebInterviewTest/index.php
+++ Eko_WebInterviewTest/index.php
@@ -1,21 +1,18 @@
-<?
-
-$page = 'home';
-
-?>
-
 <!DOCTYPE html>
+
+<?php $page = 'home'; ?>
+
 <html>
 	<head>
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="shortcut icon" href="images/favicon.ico">
 		<title>Loop</title>
-		<? include('partials/head.php'); ?>
+		<?php include('partials/head.php'); ?>
         
 	</head>
 	<body>
 
-		<? include('partials/nav.php'); ?>
+		<?php include('partials/nav.php'); ?>
 		<!-- Main jumbotron for a primary marketing message or call to action -->
 
 	    <div class="jumbotron header-jumbotron">
@@ -31,10 +28,16 @@ $page = 'home';
 
 				    
 					<p class="button-row">
-				      	<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8" target="itunes_store" button type="button" class="btn btn-default btn-store"><img src="images/badge-store-ios.svg" height="30" width="30" alt="App Store"> App Store</a>
-				      	<a href="https://play.google.com/store/apps/details?id=com.ekoapp.loop&amp;hl=en" target="_blank" button type="button" class="btn btn-default btn-store pull-right"><img src="images/badge-store-play.svg" height="30" width="30" alt="Play Store"> Play Store</a>
+				      	<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8" target="itunes_store" class="btn btn-default btn-store">
+				      		<img src="images/badge-store-ios.svg" height="30" width="30" alt="iTunes"/> App Store
+				      	</a>
+				      	
+				      	<a href="https://play.google.com/store/apps/details?id=com.ekoapp.loop&amp;hl=en" target="_blank" class="btn btn-default btn-store pull-right">
+				      		<img src="images/badge-store-play.svg" height="30" width="30" alt="GooglePlay"/> Play Store
+				      	</a>
+				      	
 				    </p>
-				    </p>
+				    
 		        </div>
 
 		        
@@ -63,8 +66,9 @@ $page = 'home';
 						<p>Saying yes or no has never been easier.<br>Add a message if you'd like.</p>
 						<p class="button-row">
 						<p>&nbsp;</p>
-				      <a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8" target="itunes_store" button type="button" class="btn btn-default btn-store"> Download For Free</a>
-					</p>
+				     	<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8" target="itunes_store" class="btn btn-default btn-store"> Download for Free
+				      	</a>
+						
 					</div>
 					
 				</div>
@@ -82,7 +86,8 @@ $page = 'home';
 						<p>All Loops are archived for easy access.<br>Never lose track of commitments again.
 							<p>&nbsp;</p>
 							<p class="button-row">
-				      		<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8", target="itunes_store" button type="button" class="btn btn-default btn-store3">Get The App</a>
+				      			<a href="https://itunes.apple.com/us/app/loop-by-eko/id931539244?mt=8" target="itunes_store" class="btn btn-default btn-store3"> Get the App
+				      			</a>
 							</p>
 					</div>
 
@@ -95,7 +100,7 @@ $page = 'home';
 		</div>
 
 		
-		<? include('partials/footer.php'); ?>
-		<? include('partials/scripts.php'); ?>
+		<?php include('partials/footer.php'); ?>
+		<?php include('partials/scripts.php'); ?>
 	</body>
 </html>
